d4d01a4031f3ca4b1cafc8640e07d95010fb7504
Hariprasad K B <hariwin7@gmail.com>
2021-10-03 09:03:09 +0530

    optional url added

--- navigation/LinkingConfiguration.ts
+++ navigation/LinkingConfiguration.ts
@@ -17,7 +17,7 @@ export default {
       ReviewDetails: "review",
       ReviewResponseDetails: "review-response",
       NotFound: "*",
-      ViewRequest: ":username/:reviewResponseId",
+      ViewRequest: ":username/:reviewResponseId?",
       SubmitSuccess: "success",
       PublishReview: "publish-review",
       SendEmails: "send-emails",
