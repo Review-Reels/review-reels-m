1cba9ee74229682ddf6aefbb167e5ef5c39e7216
Ben Babu <benkolenchery@gmail.com>
2021-11-10 08:22:31 +0530

    V1.0.8

--- screens/merchant/ProfileScreen.tsx
+++ screens/merchant/ProfileScreen.tsx
@@ -90,7 +90,7 @@ export default function ProfileScreen({
           </View>
           <View style={styles.appInfoCntnr}>
             <Text style={styles.appInfoTxt}>© Review Reels 2021</Text>
-            <Text style={styles.appInfoTxt}>V1.0.6 — Build 1.007</Text>
+            <Text style={styles.appInfoTxt}>V1.0.9 — Build 1.010</Text>
           </View>
         </View>
       </View>
