ce846b9aefa670aa9c822ac5323d5fccbcf57d37
Hariprasad K B <hariwin7@gmail.com>
2021-12-30 21:36:45 +0400

    app version update

--- constants/apiUrls.js
+++ constants/apiUrls.js
@@ -4,6 +4,7 @@
 //prod
 export const S3_URL =
   "https://review-reels-videos.s3.ap-south-1.amazonaws.com/";
+
 // export const WEB_APP_URL =
 //   "http://review-reels-frontend-test.s3-website.ap-south-1.amazonaws.com/";
 
